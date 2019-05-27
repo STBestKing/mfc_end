2b2167898593b4ebf18e076f423b1b51b4ba527f
涛 孙 <BestSunTao@outlook.com>
2019-05-27 16:36:09 +0800

    添加项目文件。

---
+++ mfc_end/mfc_end.h
@@ -0,0 +1,32 @@
+﻿
+// mfc_end.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CmfcendApp:
+// 有关此类的实现，请参阅 mfc_end.cpp
+//
+
+class CmfcendApp : public CWinApp
+{
+public:
+	CmfcendApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CmfcendApp theApp;
